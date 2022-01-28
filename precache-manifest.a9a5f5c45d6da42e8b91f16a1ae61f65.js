01f380317c43678cb7cdb04507415393d52c6366
Xiomara Garcia <xiomg1570@gmail.com>
2022-01-28 17:32:25 -0500

    Updates

---
+++ precache-manifest.a9a5f5c45d6da42e8b91f16a1ae61f65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5cae2befd36a87d89a27d0fbf8ee0f0",
+    "url": "/entrega28/index.html"
+  },
+  {
+    "revision": "cebec71e118d81b89f61",
+    "url": "/entrega28/static/css/main.239a35c9.chunk.css"
+  },
+  {
+    "revision": "f277fe791debfacb254b",
+    "url": "/entrega28/static/js/2.8a089ec2.chunk.js"
+  },
+  {
+    "revision": "cebec71e118d81b89f61",
+    "url": "/entrega28/static/js/main.2ae61a1b.chunk.js"
+  },
+  {
+    "revision": "147c6cd7c61c3243c15e",
+    "url": "/entrega28/static/js/runtime~main.71928864.js"
+  }
+]);
