7e3ebc5103f9cd6784adc3450537be932b869080
Xiomara Garcia <xiomg1570@gmail.com>
2022-01-28 15:22:47 -0500

    nueva

--- src/container/AppRouter.js
+++ src/container/AppRouter.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import Home from '../components/Home';
 import Inicio from '../components/Inicio';
+import { Html} from '../components/html/pregunta';
 
 export const AppRouter = () => {
   return <div>
       <Router >          
              <Routes>
                 {/* <Route exact path="/" component={} /> */}
-                <Route path="/home" element={<Home />} />               
+                <Route path="/home" element={<Html />} />               
                 <Route path="/inicio" element={<Inicio />} />         
              </Routes>
       </ Router >
