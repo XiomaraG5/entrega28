69a69260ba0e109634b275ad2af7e043bc9d837c
Julian Caballero <julian2895@gmail.com>
2022-01-28 09:56:30 -0500

    prueba

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,9 +1,35 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 
 export default class Inicio extends Component {
+
   render() {
-    return <div>
+    return 
+    <div>
+      
         
     </div>;
   }
-}
+}*/
+import React, { Component } from 'react';
+
+export const Navbar = () => {
+    return <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="/">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Pestaña 1</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">pestaña 2</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>;
+};
