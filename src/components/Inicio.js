f0fcd3fb5f80f395e43a2f1fdff5a4e4865f5449
Xiomara Garcia <xiomg1570@gmail.com>
2022-01-28 09:27:39 -0500

    inicio

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Cabecera extends Component {
+export default class Inicio extends Component {
   render() {
     return <div>
         
