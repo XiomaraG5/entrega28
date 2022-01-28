20f01c871200c8312c9f70703d2104eafe3fb423
Xiomara Garcia <xiomg1570@gmail.com>
2022-01-28 14:40:55 -0500

    preguntas

--- src/container/AppRouter.js
+++ src/container/AppRouter.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from '../components/Home';
+import { Html } from '../components/pregunta';
 
 export const AppRouter = () => {
   return <div>
       <Router >          
              <Routes>
                 {/* <Route exact path="/" component={} /> */}
-                <Route path="/home" element={<Home />} />        
+                <Route exact path="/home" element={<Home />} />        
+                <Route exact path="/quiz" element={<Html/>} />        
              </Routes>
       </ Router >
   </div>;
---
+++ src/styles/Preguntas.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+
+export const Form = styled.form`
+display:flex;
+flex-direction: column;
+padding: 2%;
+justify-content: center;
+width: 100%;
+ gap: 10px;
+`
+
+export const Question = styled.div`
+display:flex;
+color: #FFF;
+padding: 1%;
+justify-content: space-evenly;
+width:80%;
+background-color: #232e35;
+`
+
+export const Imagen = styled.img`
+width:25%;
+`
